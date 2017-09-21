23d2bc78052e72a32dab6bbddf62ff1c2896557e
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-09-21 00:04:43 -0600

    initial site files

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'n2QCuZbOHi3zz0yxacQlj9VHo',
+  consumer_secret: '5MsjA6SYru5U7louO2xxPSgXzglhhQdlkg2ZrB1yWxPfjRQV31',
+  access_token_key: '910735417854398464-eZsESHZu3IiuOrffkNBRnIAttWHze8q',
+  access_token_secret: 'ojTY9ZyCkPWWf6zt6H5v6jyikXwX91HwgwTZmyQtzAEkY',
+}
+
+module.exports = {
+	twitterKeys: twitterKeys
+}
