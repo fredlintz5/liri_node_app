ace50cd896e8e7b43db8ceeeea0ce6179b636d68
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-09-22 22:01:51 -0600

    updated readMe, and added program start and rerun functions

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+
 const API_Keys = require('./keys.js');
 const twitter  = require('twitter');
 const spotify  = require('node-spotify-api');
@@ -6,69 +7,99 @@ const inquirer = require('inquirer');
 const colors   = require('colors');
 
 
-inquirer.prompt([{
-    type: 'list',
-    name: 'program',
-    message: 'What program do you want to run?',
-    choices: [
-		'my-tweets', 
-		'spotify-this-song', 
-		'movie-this', 
-		'do-what-it-says\n'
-    ]
-  }
-])
-.then((answers) => {
+programStart();
 
-	switch (answers.program) {
 
-		case 'my-tweets':
-			getTweets();
-			break;
 
-		case 'spotify-this-song':
-			inquirer.prompt([{
-			    type: 'input',
-			    name: 'song',
-			    message: 'What song do you want to get info for?',
-			  }
-			])
-			.then((answers) => {
-				const song = answers.song;
-				getMusic(song);
-			});
-			break;
+function reRunProgram() {
+	inquirer.prompt([{
+	    type: 'confirm',
+	    name: 'confirm',
+	    message: 'Would you like to re-start the program?',
+	  }
+	])
+	.then((answers) => {
+		if (answers.confirm) {
+			programStart();
+		} else {
+			console.log("\nGood Bye!\n".cyan);
+		}
+	})
+}
 
-		case 'movie-this':
-			console.log('movie-this');
-			// getMovie();
-			break;
 
-		case 'do-what-it-says\n':
-			console.log('do-what-it-says');
-			break;
+function programStart() {
+	inquirer.prompt([{
+	    type: 'list',
+	    name: 'program',
+	    message: 'What program do you want to run?',
+	    choices: [
+			'my-tweets', 
+			'spotify-this-song', 
+			'movie-this', 
+			'do-what-it-says\n'
+	    ]
+	  }
+	])
+	.then((answers) => {
 
-		default:
-			console.log("You've done something wrong, try again...")
-	}
-})
+		switch (answers.program) {
 
+			case 'my-tweets':
+				getTweets();
+				break;
+
+			case 'spotify-this-song':
+				inquirer.prompt([{
+				    type: 'input',
+				    name: 'song',
+				    message: 'What song would you like info for?',
+				  }
+				])
+				.then((answers) => {
+					const song = answers.song;
+					getMusic(song);
+				});
+				break;
+
+			case 'movie-this':
+				inquirer.prompt([{
+				    type: 'input',
+				    name: 'movie',
+				    message: 'What movie would you like info for?',
+				  }
+				])
+				.then((answers) => {
+					const movie = answers.movie;
+					getMovie(movie);
+				});
+				break;
+
+			case 'do-what-it-says\n':
+				console.log('do-what-it-says');
+				break;
+
+			default:
+				console.log("You've done something wrong, try again...")
+		}
+	})
+}
 
 
 function getTweets() {
 	const client = new twitter(API_Keys.twitterKeys);
 	const queryUrl = "https://api.twitter.com/1.1/search/tweets.json?q=realFredLintz&result_type=recent&count=20";
 
-	client.get(queryUrl, function(error, tweets, response) {
+	client.get(queryUrl, (error, tweets, response) => {
 		console.log("\nHere are the latest tweets from @realFredLintz: \n".cyan);
    		
    		for (var i = 0; i < tweets.statuses.length; i++) {
-   			console.log(tweets.statuses[i].created_at.substring(0,19).green + " - " + tweets.statuses[i].text.cyan)
+   			console.log(tweets.statuses[i].created_at.substring(0,19).grey + " - " + tweets.statuses[i].text.cyan)
    		}
    		console.log('');	
 	});
+	setTimeout(reRunProgram, 1000);
 }
-
 
 
 function getMusic(song) {
@@ -83,16 +114,39 @@ function getMusic(song) {
 		let songName = data.tracks.items[0].name;
 		let songURL = data.tracks.items[0].album.artists[0].external_urls.spotify;
 		let albumName = data.tracks.items[0].album.name;
+
 		console.log("\nGreat choice!\n".cyan);
 		console.log(`I love '${songName}' by ${artistName}. Wasn't that on the album '${albumName}'?`.cyan);
-		console.log(`Have a listen over at ${songURL.green}!\n`.cyan);
-		});
+		console.log(`Have a listen over at ${songURL.grey}!\n`.cyan);
+
+		setTimeout(reRunProgram, 1000);
+	});
 }
 
 
+function getMovie(movie) {
+	const apiKey = "40e9cece";
+	const movieQueryUrl = `http://www.omdbapi.com/?t=${movie}&apikey=${apiKey}`;
 
-function getMovie() {
+	request(movieQueryUrl, (error, response, body) => {
+		if (!error && response.statusCode === 200) {
 
+			const title = JSON.parse(body).Title; 
+			const movieYear = JSON.parse(body).Year;
+			const IMDB_Rating = JSON.parse(body).Ratings[0].Value;
+			const rotten_Rating = JSON.parse(body).Ratings[1].Value;
+			const country = JSON.parse(body).Country;
+			const plot = JSON.parse(body).Plot;
+			const actors = JSON.parse(body).Actors;
+
+			console.log("\nGreat choice!\n".cyan);
+			console.log(`I love the movie '${title}', starring ${actors}. \nIt was released in ${country} in ${movieYear}, with a IMDB rating of ${IMDB_Rating}, and a Rotten Tomatoes rating of ${rotten_Rating}.\n`.cyan);
+			console.log(`In case you forgot, here's the movie's plot:\n`.cyan);
+			console.log(`${plot.grey}\n`)
+
+			setTimeout(reRunProgram, 1000);
+		}
+	});
 }
 
 
